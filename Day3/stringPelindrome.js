ba0b57ce919910d411faaa4f28f403a4454b37f6
sohan paliyal <sohnpaliyal@gmail.com>
2023-07-26 20:36:20 +0530

    day3 string pelindrome

---
+++ Day3/stringPelindrome.js
@@ -0,0 +1,22 @@
+
+
+function checkPelindrome(str){
+    let firstIndex = 0;
+    let lastIndex = str.length - 1;
+
+
+    while(firstIndex< lastIndex){
+        if(str[firstIndex] !== str[lastIndex]){
+            return false;
+        }
+
+        firstIndex++;
+        lastIndex--;
+    }
+
+    return true
+}
+
+const isStringPel = checkPelindrome('naman')
+
+console.log("isStringPel ==>", isStringPel);
