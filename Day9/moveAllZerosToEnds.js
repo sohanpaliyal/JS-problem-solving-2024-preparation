8936e1b0d5d6eeca80b89d83abf60df997385b3c
sohan paliyal <sohnpaliyal@gmail.com>
2023-09-18 20:59:34 +0530

    move all zeros to end brute force solution

--- Day9/moveAllZerosToEnds.js
+++ Day9/moveAllZerosToEnds.js
@@ -6,8 +6,9 @@ const moveAllZeros = (arr) =>{
     let zeros = 0;
     for (let i = 0; i < arr.length; i++) {
         if(arr[i]!==0 ){
-            zeros++;
             tempArr.push(arr[i])
+        }else{
+            zeros++;
         }
     }
 
