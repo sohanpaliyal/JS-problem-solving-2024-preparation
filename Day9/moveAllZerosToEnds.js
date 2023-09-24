a19354627d679ec9c1a8004506c89fa31bfc0510
sohan paliyal <sohnpaliyal@gmail.com>
2023-09-24 11:37:02 +0530

    optimal solution moveAllZerosToEnd

--- Day9/moveAllZerosToEnds.js
+++ Day9/moveAllZerosToEnds.js
@@ -1,21 +1,53 @@
-const arr =[ 2,5,5, 0, 0, 2456 ,630 ,0,50,30,0, 0,0];
+const arr = [2, 5, 5, 0, 0, 2456];
 
+// brute force 
+const moveAllZeros = (arr) => {
+    // const tempArr = [];
+    // let zeros = 0;
+    // for (let i = 0; i < arr.length; i++) {
+    //     if(arr[i]!==0 ){
+    //         tempArr.push(arr[i])
+    //     }else{
+    //         zeros++;
+    //     }
+    // }
 
-const moveAllZeros = (arr) =>{
-    const tempArr = [];
-    let zeros = 0;
-    for (let i = 0; i < arr.length; i++) {
-        if(arr[i]!==0 ){
-            tempArr.push(arr[i])
-        }else{
-            zeros++;
-        }
-    }
-
-    for (let i = 0; i < zeros; i++) {
-        tempArr.push(0)
-    }
-    return tempArr;
+    // for (let i = 0; i < zeros; i++) {
+    //     tempArr.push(0)
+    // }
+    // return tempArr;
 }
 
-console.log("moveAllZeros(arr) ===>",moveAllZeros(arr))
+
+
+    // two pointer approach 
+
+// optimal solution
+    const moveAllZerosToEnd = (arr) => {
+        let firstZero = -1;
+
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === 0) {
+                firstZero = i;
+                break;
+            }
+        }
+
+        if (firstZero < 0) return arr;
+
+
+        for (let i = firstZero + 1; i < arr.length; i++) {
+            if (arr[i] !== 0) {
+                const temp = arr[i];
+                arr[i] = arr[firstZero];
+                arr[firstZero] = temp;
+                firstZero++;
+            }
+        }
+        return arr;
+    }
+
+
+    console.log("moveAllZeros(arr) ===>", moveAllZerosToEnd(arr))
+
+
